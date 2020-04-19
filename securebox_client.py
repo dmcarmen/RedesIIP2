822a3d20ba7b2b18170cbe4934f0a843425a1f0f
rrirr <irisalvz@gmail.com>
2020-04-19 16:31:28 +0200

    feten

--- securebox_client.py
+++ securebox_client.py
@@ -5,7 +5,6 @@ from Cryptodome.Random import get_random_bytes
 from Cryptodome.Cipher import AES
 from Cryptodome.Util.Padding import pad, unpad
 from Cryptodome.Cipher import PKCS1_OAEP
-#result = json.dumps({'iv':iv, 'ciphertext':ct})
 #TODO AES.CBC puede dar excepcion?
 
 def encrypt(mensaje, clave_pub_r):
@@ -28,35 +27,52 @@ def encrypt(mensaje, clave_pub_r):
     except(ValueError):
         return None
 
-def sign(mensaje, clave_priv_e):
+def sign(mensaje):
     # Hacemos hash256 del mensaje
     hash = SHA256.new(mensaje)
     # Ciframos con PKCS 1.5 el hash con la clave privada del emisor
     try:
+        #Obtenemos la clave privada
+        f = open('clave.pem','r')
+        clave_priv_e = RSA.import_key(f.read())
+        f.close()
+
         # sign raises ValueError y TypeError
         firma_digital = pkcs1_15.new(clave_priv_e).sign(hash)
         return firma_digital
     except (ValueError, TypeError):
         return None
 
-def enc_sign(mensaje, clave_priv_e, clave_pub_r):
-    firma_digital = sign(mensaje, clave_priv_e)
+def enc_sign(mensaje, clave_pub_r):
+    firma_digital = sign(mensaje)
 
     mensaje_enc_sign = encrypt(firma_digital + mensaje, clave_pub_r)
 
     return mensaje_enc_sign
 
-def check_sign_and_decrypt(mensaje, clave_pub_e, clave_priv_r):
+def decrypt(mensaje):
+    print("-> Descifrando fichero...", end="")
     iv = mensaje[0:16]
     sobre_digital = mensaje[16:16+256]
 
+    # Obtenemos la clave privada del receptor
+    f = open('clave.pem','r')
+    clave_priv_r = RSA.import_key(f.read())
+
     # Obtenemos el sobre con OAEP
-    # La clave es la privada del receptor
     cipher = PKCS1_OAEP.new(clave_priv_r)
     clave_s = cipher.decrypt(sobre_digital)
 
     cipher = AES.new(clave_s, AES.MODE_CBC, iv)
     mensaje_descifrado = unpad(cipher.decrypt(mensaje[16+256:]), AES.block_size)
+
+    f.close()
+    print("OK")
+    return mensaje_descifrado
+
+def check_sign(mensaje_descifrado, clave_pub_e):
+    print("-> Verificando firma...", end="")
+
     firma_digital = mensaje_descifrado[:256]
     mensaje_original = mensaje_descifrado[256:]
 
@@ -65,17 +81,20 @@ def check_sign_and_decrypt(mensaje, clave_pub_e, clave_priv_r):
 
     #Comprobamos la firma_digital con el hash
     pkcs1_15.new(clave_pub_e).verify(hash, firma_digital)
+    print("OK")
     return mensaje_original
 
 
 new_key = RSA.generate(2048)
+f = open('clave.pem','wb')
+f.write(new_key.export_key('PEM'))
+f.close()
 public_key = new_key.publickey()
 private_key = new_key
 clave_pub_e=public_key
-clave_priv_e=private_key
 clave_pub_r=public_key
-clave_priv_r=private_key
-file= open("Prueba.txt","r")
+file= open("Prueba.txt","rb")
 mensaje=file.read()
-encriptado= enc_sign(mensaje.encode("utf-8"),clave_priv_e ,clave_pub_r)
-print(check_sign_and_decrypt(encriptado,clave_pub_e,clave_priv_r).decode("utf-8"))
+encriptado=enc_sign(mensaje,clave_pub_r)
+mensaje_descifrado= decrypt(encriptado)
+print(check_sign(mensaje_descifrado,clave_pub_e).decode("utf-8"))
