a78a88c9ed21aa13d845c7748ffa6c51fab5f0c0
Carmen <carmen.diezmenendez@estudiante.uam.es>
2020-04-21 14:31:33 -0400

    completado path fichero y comentarios

--- files.py
+++ files.py
@@ -1,5 +1,6 @@
 import requests
 import utils as u
+import os
 
 u.config_ini()
 headers = {'Authorization': "Bearer " + u.token}
@@ -7,49 +8,83 @@ urlIni = 'https://tfg.eps.uam.es:8080/api/files/'
 path_archivos = "Archivos/"
 
 def upload(dest_id, file_path):
+    '''
+        Nombre: upload
+        Descripcion: Envia un fichero a otro usuario.
+            Subimos el archivo a SecureBox firmado y cifrado con las claves adecuadas
+            para que pueda ser recuperado y verificado por el destinatario.
+            En caso de error lo imprime por pantalla y sale de la funcion.
+        Argumentos:
+            -dest_id: ID del user que recibira el archivo.
+            -file_path: path del archivo a subir.
+        Retorno: Ninguno
+    '''
+
+    print("Solicitando envio de fichero a SecureBox")
+
+    # Abrimos el archivo y vemos donde guardar el cifrado y firmado temporalmente
     f = open(file_path, "rb")
     mensaje = f.read()
     f.close()
     file_name = os.path.basename(file_path)
-    path_archivo = path_archivos+file_name
+    path_archivo = os.path.abspath(path_archivos + file_name)
 
-    print("Solicitando envio de fichero a SecureBox")
-    mensaje_sign = sign(mensaje)
-    if sign is None:
-        return
-
+    # Conseguimos la clave publica de dest_id
     print("Recuperando clave pública de ID {}...".format(dest_id), end="")
     clave_pub_r = get_public_key(dest_id)
+    if clave_pub_r is None:
+        return
     print("OK")
 
-    mensaje_enc_sign = encrypt(mensaje, clave_pub_r)
+    # Firmamos y ciframos el fichero
+    print("Firmando y cifrando el fichero...", end="")
+    mensaje_enc_sign = enc_sign(mensaje, clave_pub_r)
+    if mensaje_enc_sign is None:
+        return
+    print("OK")
 
     # Guardamos el fichero encriptado y firmado
-    f = open(path_archivo,"wb")
+    f = open(path_archivo, "wb")
     f.write(mensaje_enc_sign)
-    f.close()
 
-    #TODO ufile=@/home/  / .py con path_archivo
+    # Enviamos la peticion a la API
     print("Subiendo fichero a servidor...", end="")
     url = urlIni + "upload"
+    files = {'ufile': f}
     try:
-        r = requests.post(url, headers=headers)
+        r = requests.post(url, headers=headers, files=files)
     except requests.ConnectionError:
         print("\nError de conexion")
         return
 
+    # Si es correcto imprimimos el ID del archivo
     if r.status_code == requests.codes.ok:
         print("OK")
         answers = r.json()
         files_id = answers.get('files_id')
         file_size = answers.get('file_size') #TODO que hacer con file_size?
         print("Subida realizada correctamente, ID del fichero: {}".format(files_id))
+        # Borramos el archivo auxiliar
+        #os.remove(path_archivo)
     else:
+        print()
         u.error(r)
 
 
 def download(file_id, source_id):
+    '''
+        Nombre: download
+        Descripcion: Recupera un fichero con ID id_fichero del sistema.
+            Tras ser descargado, desciframos el contenido y verificamos la firma.
+            En caso de error lo imprime por pantalla y sale de la funcion.
+        Argumentos:
+            -file_id: ID del fichero en el sistema.
+            -source_id: ID del user al que pertenece.
+        Retorno: Ninguno
+    '''
     print("Descargando fichero de SecureBox...", end="")
+
+    # Enviamos la peticion a la API
     url = urlIni + "download"
     args = {'file_id': file_id}
     try:
@@ -58,6 +93,7 @@ def download(file_id, source_id):
         print("Error de conexion")
         return
 
+    # Si es correcto recuperamos el mensaje
     if r.status_code == requests.codes.ok:
         print("OK")
         print("-> {} bytes descargados correctamente".format(len(r)))
@@ -68,6 +104,7 @@ def download(file_id, source_id):
             return
         print("OK")
 
+        # Recuperamos la clave publica del usuario y verificamos la firma
         print("-> Recuperando clave pública de ID {}...".format(source_id))
         clave_pub_e = get_public_key(source_id)
         print("OK")
@@ -78,13 +115,25 @@ def download(file_id, source_id):
             return
         print("OK")
 
-        #TODO guardar el fichero
+        # Guardamos el fichero en path_archivos
+        f = open(file_path + r.headers.get('filename'), 'wb')
+        f.write(r)
 
         print("Fichero descargado y verificado correctamente")
     else:
+        print()
         u.error(r)
 
 def delete(file_id):
+    '''
+        Nombre: delete
+        Descripcion: Borra un fichero del sistema.
+            En caso de error lo imprime por pantalla y sale de la funcion.
+        Argumentos:
+            -file_id: ID del fichero en el sistema.
+        Retorno: Ninguno
+    '''
+    # Enviamos la peticion a la API
     url = urlIni + "delete"
     args = {'file_id': file_id}
     print("Solicitando borrado del fichero #{}...".format(file_id),end="")
@@ -100,9 +149,19 @@ def delete(file_id):
         file_id = answer.get('file_id')
         print("Fichero con ID#{} borrado correctamente".format(file_id))
     else:
+        print()
         u.error(r)
 
 def list_files(userID):
+    '''
+        Nombre: list_files
+        Descripcion: Lista todos los ficheros pertenecientes al usuario.
+            En caso de error lo imprime por pantalla y sale de la funcion.
+        Argumentos:
+            -userID: ID del fichero en el sistema.
+        Retorno: Ninguno
+    '''
+    # Enviamos la peticion a la API
     url = urlIni + "list"
     args = {'userID': userID}
     print("Buscando ficheros del usuario #{} en el servidor".format(userID),end="")
@@ -112,6 +171,7 @@ def list_files(userID):
         print("\nError de conexion")
         return
 
+    # Si es correcta impimimos todos los ficheros del usuario
     if r.status_code == requests.codes.ok:
         print("OK")
         answers = r.json()
@@ -123,4 +183,5 @@ def list_files(userID):
             print("[{}] #{}".format(i,file))
             i+=1
     else:
+        print()
         u.error(r)
